d1961c6700332a498ece60e536aeb0dc37a6fd21
Deniz Tuna <deniztunatr@gmail.com>
2022-07-14 14:33:12 +0200

    styles updated

--- screens/WrongAnswerScreen.tsx
+++ screens/WrongAnswerScreen.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function WrongAnswerScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Try Again!</Text>
-      <Text style={styles.paragraph}>Your answer is incorrect, but don't worry!</Text>
+      <Text style={styles.title}>Try again!</Text>
+      <Text style={styles.paragraph}>Your answer is incorrect!</Text>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Details</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -16,17 +19,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fff',
-    height: '100%'
+    height: '100%',
+    padding: 10
   },
   title: {
     fontSize: 35,
     fontWeight: 'bold',
-    color: '#000',
     marginBottom: 33,
-    marginTop: 33
+    marginTop: 33,
+    color: '#F67D71'
   },
   paragraph: {
-    fontSize: 21,
-    color: '#000'
+    fontSize: 21
+  },
+  button: {
+    backgroundColor: '#F67D71',
+    height: 60,
+    width: '90%',
+    borderRadius: 20,
+    marginTop: 10,
+    marginBottom: 33,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'flex'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 30,
+    textAlign: 'center',
+    lineHeight: 60
   }
 });
